a56c2a8ba301595a5491b0d084046cdb647b7ca5
nsayed <nsayed@ntgclarity.com>
2026-09-16 00:56:44 +0300

    update locatiopn and some details

--- details.js
+++ details.js
@@ -2,6 +2,6 @@
 window.invitationDetails = {
  heading: 'حفل خطوبة', name: 'سفيان - الاء', dateLabel: '30.09.2026',
  day: 'الأربعاء', timeLabel: 'الساعة 8:00 مساءً', venue: 'قاعة Revira',
- eventDate: '2026-09-30T20:00:00+03:00', mapUrl: 'https://maps.app.goo.gl/45eAyonvs1WmnS8v6',
+ eventDate: '2026-09-30T20:00:00+03:00', mapUrl: 'https://maps.app.goo.gl/E1JhASUZYyAjgf1w6',
  musicUrl: 'assets/invitation-music.mp3'
 };
