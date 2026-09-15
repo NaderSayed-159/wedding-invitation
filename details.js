4b00f90b0c483f52988bef0bd1511d9760479415
nsayed <nsayed@ntgclarity.com>
2026-09-16 01:00:15 +0300

    update hall name

--- details.js
+++ details.js
@@ -1,7 +1,7 @@
 // Replace these sample details with the final invitation information.
 window.invitationDetails = {
  heading: 'حفل خطوبة', name: 'سفيان - الاء', dateLabel: '30.09.2026',
- day: 'الأربعاء', timeLabel: 'الساعة 8:00 مساءً', venue: 'قاعة Revira',
+ day: 'الأربعاء', timeLabel: 'الساعة 8:00 مساءً', venue: 'قاعة The view - نادي الجلاء',
  eventDate: '2026-09-30T20:00:00+03:00', mapUrl: 'https://maps.app.goo.gl/E1JhASUZYyAjgf1w6',
  musicUrl: 'assets/invitation-music.mp3'
 };
